72ba1f454ad9b5d40ac273ed9a668979d1d9a3fe
igordu18 <igordu.souza@gmail.com>
2023-08-28 11:02:59 -0300

    fix: Add ref to TextInput and adjust some styles

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -7,5 +7,6 @@ export * from './components/MultiStep'
 export * from './components/Text'
 export * from './components/TextArea'
 export * from './components/TextInput'
+export * from './styles'
 // eslint-disable-next-line prettier/prettier
 
